7cc2431448283bb9da19cf653c7cba88b04e35a5
Niyi1983 <142004482+Niyi1983@users.noreply.github.com>
2023-09-26 05:36:34 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -32,5 +32,5 @@ variable "instance_type" {
 variable "instance_keypair" {
   description = "AWS EC2 Key Pair that need to be associated with EC2 Instance"
   type = string
-  default = "TJkeypair.pem" 
+  default = "TJkeypair-kp" 
 }
