bf093166bc24d57e0d8f83936cdc08e7caf1ca74
Niyi1983 <142004482+Niyi1983@users.noreply.github.com>
2023-10-22 10:07:55 -0700

    Update jenkins-svr.tf

--- jenkins-svr.tf
+++ jenkins-svr.tf
@@ -11,7 +11,7 @@ data "aws_ami" "latest-amazon-linux-image" {
   }
 }
 
-resource "aws_instance" "demo-server" {
+resource "aws_instance" "sandbox4-server" {
   ami                         = data.aws_ami.latest-amazon-linux-image.id
   instance_type               = var.instance_type
   key_name                    = var.instance_keypair
